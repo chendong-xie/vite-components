bf7b4dcde576ad1dd5b69f85808b886c77f6393e
LAPTOP-RE7N9HEU\chend <chendong.xie@sayweee.com>
2022-08-29 14:15:51 +0800

    feat: add storebook eslint prettier

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,75 @@
-const Button = (): JSX.Element => {
-  return (
-    <div className="w-24 inline-blick bg-gray-200 rounded-full">Button</div>
+import classNames from 'classnames';
+import { FC, PropsWithChildren } from 'react';
+import styles from './index.module.css';
+
+type Props = {
+  /** 类型 */
+  type?: 'default' | 'primary' | 'link';
+  /** 形状 */
+  shape?: 'default' | 'circle';
+  /**大小 */
+  size?: 'default' | 'small' | 'large';
+  /** 属性将使按钮适合其父宽度 */
+  block?: boolean;
+  /**头部插槽 */
+  prefix?: React.ReactNode;
+  /**尾部插槽 */
+  suffix?: React.ReactNode;
+  /**是否有点击动画 */
+  animated?: boolean;
+};
+
+export type ButtonProps = PropsWithChildren<
+  Props & Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'type' | 'prefix'>
+>;
+
+const Button: FC<ButtonProps> = ({
+  type = 'default',
+  size = 'default',
+  shape = 'default',
+  block,
+  prefix,
+  suffix,
+  disabled,
+  className,
+  animated = true,
+  children,
+  ...rest
+}) => {
+  // hooks start
+
+  // hooks end
+
+  // useEffect functions start
+
+  // useEffect functions end
+
+  // logic functions start
+  const classes = classNames(
+    styles.button,
+    'disabled:bg-[#999999] disabled:border-[#999999]  disabled:cursor-not-allowed',
+    className,
+    {
+      [styles[`button-${type}`]]: type,
+      [styles[`button-size-${size}`]]: size,
+      [styles[`button-shape-${shape}`]]: shape,
+      [styles['button-block']]: block,
+      [styles['button-no-animation']]: !animated || disabled,
+    },
   );
+  // logic functions end
+
+  // render functions start
+  return (
+    <>
+      <button className={classes} disabled={disabled} {...rest}>
+        {prefix && prefix}
+        {children}
+        {suffix && suffix}
+      </button>
+    </>
+  );
+  // render functions end
 };
 
 export default Button;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "tailwindcss/tailwind.css";
-import Button from "./components/Button";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'tailwindcss/tailwind.css';
+import Button from './components/Button';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Button />
-  </React.StrictMode>
+    <Button size='large'>test</Button> 
+  </React.StrictMode>,
 );
